4a11b8b4a242c7a81ee104f52ac26559aa6edbf7
GencoMattia <gencomattia@hotmail.it>
2024-06-07 14:36:31 +0200

    imported AppHero into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script setup>
-import AppTitle from "./components/AppTitle.vue"
+import AppTitle from "./components/AppTitle.vue";
+import AppHero from "./components/AppHero.vue";
 
 components: {
   AppTitle
+  AppHero
 }
 </script>
 
 <template>
 
 <AppTitle />
-
+<main>
+  <AppHero />
+</main>
 </template>
 
 <style scoped>
