743525d233915c1e60bea47c5fb0e02d16bb96f9
GencoMattia <gencomattia@hotmail.it>
2024-06-07 14:33:53 +0200

    created new component AppHero: added sction jumbotron > div.title > h2, div.img

---
+++ src/components/AppHero.vue
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<template>
+    <section class="jumbotron">
+        <div class="jumbotron_title">
+            <h2>
+                Questo è il titolo del Jumbotron!
+            </h2>
+        </div>
+        <div class="jumbotron_img">
+
+        </div>
+    </section>
+</template>
+
+<style>
+
+</style>
